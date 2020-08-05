f2e717df3934618183d1a74d54459996e9f19dfe
Marija <maria5stojanovic@gmail.com>
2020-08-04 20:55:19 -0400

    bcrypt implement

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const ejs= require("ejs")
 const bodyParser=require("body-parser")
 const app= express()
 const mongoose =require("mongoose")
-const encrypt=require("mongoose-encryption")
+const bcrypt=require("bcrypt")
+const saltRounds=10
+
+
+
+
 app.use(express.static("public"))
 app.set('view engine',"ejs")
 app.use(bodyParser.urlencoded({
@@ -17,7 +22,6 @@ const userSchema= new mongoose.Schema({
     password: String
 })
 
-userSchema.plugin(encrypt,{secret: process.env.SECRET,encryptedFields: ["password"]})
 
 const User=mongoose.model("User",userSchema)
 
@@ -33,17 +37,20 @@ app.get("/register",function(req,res){
     res.render("register")
 })
 app.post("/register",function(req,res){
-    const newUser=new User({
-        email: req.body.username,
-        password: req.body.password
+    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+        const newUser=new User({
+            email: req.body.username,
+            password: hash
+        })
+        newUser.save(function(err){
+            if(err){
+                console.log(err)
+            }else {
+                res.render("secrets")
+            }
+        })
     })
-    newUser.save(function(err){
-        if(err){
-            console.log(err)
-        }else {
-            res.render("secrets")
-        }
-    })
+    
 })
 app.post("/login",function(req,res){
     const username=req.body.username
@@ -53,10 +60,11 @@ app.post("/login",function(req,res){
             console.log(err)
         }else {
             if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets")
-
-                }
+                bcrypt.compare(password,foundUser.password,function(req,result){
+                    if(result === true){
+                        res.render("secrets")
+                    }
+                })
             }
         }
     })
